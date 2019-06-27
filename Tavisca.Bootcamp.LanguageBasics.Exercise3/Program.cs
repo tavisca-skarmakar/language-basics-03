40a88edab42b8068db9c01864c533874c12a2180
tavisca-skarmakar <skarmakar@tavisca.in>
2019-06-27 11:24:34 +0530

    Added implementation of SelectMeals

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -8,22 +9,22 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         static void Main(string[] args)
         {
             Test(
-                new[] { 3, 4 }, 
-                new[] { 2, 8 }, 
-                new[] { 5, 2 }, 
-                new[] { "P", "p", "C", "c", "F", "f", "T", "t" }, 
+                new[] { 3, 4 },
+                new[] { 2, 8 },
+                new[] { 5, 2 },
+                new[] { "P", "p", "C", "c", "F", "f", "T", "t" },
                 new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
             Test(
-                new[] { 3, 4, 1, 5 }, 
-                new[] { 2, 8, 5, 1 }, 
-                new[] { 5, 2, 4, 4 }, 
-                new[] { "tFc", "tF", "Ftc" }, 
+                new[] { 3, 4, 1, 5 },
+                new[] { 2, 8, 5, 1 },
+                new[] { 5, 2, 4, 4 },
+                new[] { "tFc", "tF", "Ftc" },
                 new[] { 3, 2, 0 });
             Test(
-                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 }, 
-                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 }, 
-                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 }, 
-                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" }, 
+                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 },
+                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 },
+                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 },
+                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" },
                 new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
             Console.ReadKey(true);
         }
@@ -40,8 +41,118 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int[] calorie = new int[protein.Length];
+            var dietList = new List<int>();
+
+            for (int i = 0; i < protein.Length; i++)
+            {
+                calorie[i] = 5 * carbs[i] + 5 * protein[i] + 9 * fat[i];
+            }
+
+            var index = new List<int>();
+
+            for (int i = 0; i < dietPlans.Length; i++)
+            {
+                index.Clear();
+                if (dietPlans[i].Length == 0)
+                {
+                    index.Clear();
+                    index.Add(0);
+                }
+                else
+                {
+                    foreach (var ch in dietPlans[i])
+                    {
+                        if (index.Count == 1) break;
+                        UpdateIndexList(protein, carbs, fat, calorie, ch, index);
+                    }
+                }
+
+                dietList.Add(index[0]);
+            }
+
+            int[] diet = new int[dietPlans.Length];
+            dietList.CopyTo(diet);
+            dietList.Clear();
+
+            return diet;
+        }
+
+        public static void UpdateIndexList(int[] protein, int[] carbs, int[] fat, int[] calorie, char ch, List<int> index)
+        {
+            switch (ch)
+            {
+                case 'C': FindIndex(carbs, index, "max"); break;
+                case 'c': FindIndex(carbs, index, "min"); break;
+                case 'P': FindIndex(protein, index, "max"); break;
+                case 'p': FindIndex(protein, index, "min"); break;
+                case 'F': FindIndex(fat, index, "max"); break;
+                case 'f': FindIndex(fat, index, "min"); break;
+                case 'T': FindIndex(calorie, index, "max"); break;
+                case 't': FindIndex(calorie, index, "min"); break;
+            }
+        }
+
+        public static void FindIndex(int[] arr, List<int> index, string typeOfIndex)
+        {
+            var indexList = new List<int>();
+            int arrayValue;
+
+            if (typeOfIndex.Equals("min"))
+            {
+                arrayValue = int.MaxValue;
+            }
+            else
+            {
+                arrayValue = int.MinValue;
+            }
+
+            if (index.Count == 0)
+            {
+
+                indexList.Clear();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arrayValue == arr[i]) indexList.Add(i);
+
+                    if (CheckMinMax(arrayValue, arr[i], typeOfIndex))
+                    {
+                        indexList.Clear();
+                        arrayValue = arr[i];
+                        indexList.Add(i);
+                    }
+                }
+            }
+            else
+            {
+                indexList.Clear();
+                foreach (var i in index)
+                {
+                    if (arrayValue == arr[i]) indexList.Add(i);
+
+                    if (CheckMinMax(arrayValue, arr[i], typeOfIndex))
+                    {
+                        indexList.Clear();
+                        arrayValue = arr[i];
+                        indexList.Add(i);
+                    }
+                }
+            }
+            index.Clear();
+            for (int i = 0; i < indexList.Count; i++)
+                index.Add(indexList[i]);
+        }
+
+        public static bool CheckMinMax(int arrayValue, int arrElement, string operation)
+        {
+            if (operation.Equals("min"))
+            {
+                return (arrayValue > arrElement) ? true : false;
+            }
+            else
+            {
+                return (arrayValue < arrElement) ? true : false;
+            }
         }
     }
 }
